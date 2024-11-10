1d815739112c4ea1f495a0f7639c1ad998726d22
Simoon Song <simoonsong1006@gmail.com>
2024-11-10 10:56:09 -0800

    Made the iphone frame layout

---
+++ src/app/components/PhoneFrame.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PhoneFrame = () => {
+	return (
+		<div className="flex justify-center items-center p-8">
+			<div className="w-[350px] h-[700px] border-4 border-gray-500 bg-gray-100 rounded-[45px]">
+				<div className="w-full overflow-hidden">
+					<p className="text-center text-gray-700">iPhone Screen</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default PhoneFrame;
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import PhoneFrame from "./components/PhoneFrame";
+
+export default function Home() {
+	return (
+		<div>
+			<PhoneFrame />
+		</div>
+	);
+}
