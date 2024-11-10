5c9099c4a39d0117677ec8e7f0d8317c0b525147
sshahr02 <sshahr02@students.cs.ubc.ca>
2024-11-10 14:04:23 -0800

    Task button in CSS before Tailwind CSS

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./task_object.css";
+import { MdRadioButtonUnchecked } from "react-icons/md";
+import { FaRegTrashCan } from "react-icons/fa6";
 
-const Tasks = () => {
-	return <div>Tasks</div>;
+const taskBubble = () => {
+  return <div className="task_button">
+    <p className="button_text">This is a task name</p>
+    <MdRadioButtonUnchecked className="check_circle"/>
+    <FaRegTrashCan className="trashcan"/>
+  </div>;
 };
 
-export default Tasks;
+
+export default taskBubble;
