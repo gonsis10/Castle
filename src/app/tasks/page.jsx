bb58bd029411bc4e2461a9af20301899f7c49af4
sshahr02 <sshahr02@students.cs.ubc.ca>
2024-11-10 17:09:11 -0800

    Added the ability for Page's render for task bubble to be either finished or not finished

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import TaskBubble from "./Task.jsx";
 
-const Page = () => {
+const Page = ({name, IsFinished}) => {
+    // const [IsFinished, setFinished] = useState(false)
+    
     return (
         <div>
-            <TaskBubble name="Task name" IsChecked="true"/>
+            <TaskBubble name={name} IsFinished={IsFinished}/>
         </div>
     )
 }
