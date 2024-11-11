711aee8ffce32b87385f04f2c0699ccff7732488
Zoe Shong <zshong01@students.cs.ubc.ca>
2024-11-10 18:54:02 -0800

    current level display

--- src/app/components/ProgressBar.jsx
+++ src/app/components/ProgressBar.jsx
@@ -32,7 +32,8 @@ import "../tasks/tasks.css";
 		};
 	}, [user]);
 
-    let percentage = (score/maxValue)*100
+    let percentage = (score % 20/maxValue)*100
+    
     return (
         <div className="flex flex-row items-center px-4 gap-4 select-none">
             <span className="whitespace-nowrap font-semibold text-sm w-5">{percentage}%</span>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,29 +5,32 @@ import GoogleSignIn from "./components/GoogleSignIn";
 import { useAuth } from "./context/AuthContext";
 import { useRouter } from "next/navigation";
 import ProgressBar from "./components/ProgressBar";
-import { Castle } from "lucide-react";
+import CurrentLevel from "./components/CurrentLevel";
 
 export default function Home() {
-	const { user, setUser } = useAuth();
+    const { user, setUser } = useAuth();
 
-	const handleSignInSuccess = (userData) => {
-		setUser(userData);
-		console.log(user);
-	};
+    const handleSignInSuccess = (userData) => {
+        setUser(userData);
+        console.log(user);
+    };
 
-	return (
-		<div className="App">
-			{user ? (
-				<div className="">
-					<h1>Hello {user["displayName"]}!</h1>
+    return (
+        <div className="App">
+            {user ? (
+                <div className="">
+                    <h1>Hello {user["displayName"]}!</h1>
+                    <div>
+                        <ProgressBar maxValue="20" />
+                    </div>
+                    <div>
+                        <CurrentLevel maxValue="20" />
+                    </div>
 
-					<div className="">
-						<ProgressBar maxValue="100" />
-					</div>
-				</div>
-			) : (
-				<GoogleSignIn onSignInSuccess={handleSignInSuccess} onSignInError={(error) => alert(error.message)} />
-			)}
-		</div>
-	);
+                </div>
+            ) : (
+                <GoogleSignIn onSignInSuccess={handleSignInSuccess} onSignInError={(error) => alert(error.message)} />
+            )}
+        </div>
+    );
 }
